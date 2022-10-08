ca17d921a2898f185fb9be42e21271c356090ccb
patelanandec54132 <patelanandec54132@gmail.com>
2022-10-08 14:31:29 +0530

    output corrected

--- mask1.m
+++ mask1.m
@@ -21,8 +21,9 @@ maskvalue=ones(masksize,masksize);
 weightmask=sum(sum(maskvalue));
 for i=(masksize+1)/2:m+(masksize-1)/2% (size(mask,1)-1)/2:(m+(size(mask,1)+1)/2)
     for j=(masksize+1)/2:n+(masksize-1)/2
-        z(i-2,j-2)=sum(sum((y(i-(masksize-1)/2:i+(masksize-1)/2,j-(masksize-1)/2:j+(masksize-1)/2).*maskvalue)))/weightmask;
+        z(i-(masksize-1)/2,j-(masksize-1)/2)=sum(sum((y(i-(masksize-1)/2:i+(masksize-1)/2,j-(masksize-1)/2:j+(masksize-1)/2).*maskvalue)))/weightmask;
     end
 end
-figure(2);imshow(uint8(z));
+z=uint8(z);% convert double to unit8
+figure(2);imshow(z);
 end
